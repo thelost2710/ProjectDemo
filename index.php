8db48fd781242217f6e847749651b1dda18cc361
Hai Phong <phongnth20@gmail.com>
2019-10-07 14:28:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,55 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>PHP Page</title>
-    </head>
-    <body>
-        <h1>My first PHP page</h1>
-		
-		<?php
-		
-			echo "show all rows from postgres database";
-			
-			$db = parse_url(getenv("DATABASE_URL"));
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-			$pdo = new PDO("pgsql:" . sprintf(
-			"host=%s;port=%s;user=%s;password=%s;dbname=%s",
-			$db["host"],
-			$db["port"],
-			$db["user"],
-			$db["pass"],
-			ltrim($db["path"], "/")
-	));
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/customcss.css">
+    <!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.0/css/mdb.min.css" rel="stylesheet">
+    <title>Product Database</title>
+  </head>
+<body>
+
+<h1>Product Page</h1>
+
+<?php
+	echo "Showing all rows from Postgres Database";
 	
-	$sql = "select studentname, course from RegisterCourse"
-	$stmt = $pdo ->prepare($sql);
-	
-	$stmt->setFetchMode(PDO::ASSOC);
+	//Refere to database 
+	$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+	//your sql query
+	$sql = "SELECT * FROM Product";
+	$stmt = $pdo->prepare($sql);
+	//execute the query on the server and return the result set
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
 	$stmt->execute();
 	$resultSet = $stmt->fetchAll();
+	//display the data 
+?>
+<ul>
+	<?php
+		foreach ($resultSet as $row) {
+			echo "<li>" .
+				$row["productID"] . '--'. $row["name"] . '--'. $row["price"]
+			. "</li>";
+		}
 	?>
-	<ul>
-		<?php
-		foreach($resultSet as $row){
-		echo "<li>".
-			$row["studentname"] . '--'. $row["course"]
-			."<li>";
-			}
-			?>
-    </body>
+</ul>
+
+</body>
 </html>
