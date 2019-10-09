5166a18750e136b11cbcb58a821b1da7f059ccee
Hai Phong <phongnth20@gmail.com>
2019-10-09 14:25:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,21 +19,21 @@
 <h1>Product Page</h1>
 
 <?php
-	echo "Showing all rows from Product Database";
+	echo "Show all rows from Postgres Database";
 	
 	//Refere to database 
 	$db = parse_url(getenv("DATABASE_URL"));
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
+	//you sql query
+	$sql = "SELECT studentname, course FROM registercourse";
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-	//your sql query
-	$sql = "SELECT * FROM Product";
 	$stmt = $pdo->prepare($sql);
 	//execute the query on the server and return the result set
 	$stmt->setFetchMode(PDO::FETCH_ASSOC);
@@ -45,7 +45,7 @@ $pdo = new PDO("pgsql:" . sprintf(
 	<?php
 		foreach ($resultSet as $row) {
 			echo "<li>" .
-				$row["productID"] . '--'. $row["name"] . '--'. $row["price"]
+				$row["studentname"] . '--'. $row["course"]
 			. "</li>";
 		}
 	?>
